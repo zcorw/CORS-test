fb42e2f6938c6a38b7459fa8e535f620d740272c
zcorw <zhzhwwt@qq.com>
2019-03-10 23:03:39 +0800

    修复cookie上传错误

--- server.js
+++ server.js
@@ -10,23 +10,32 @@ http.createServer(function (req, res) {
 
 http.createServer(function (req, res) {
     const [mod, action, type] = req.url.match(/(\w+)/g) || [];
+    const result = "Hello, World";
     if (mod == "simple") {
         if (action == "success") {
             response(res)
-                .setHeader("Access-Control-Allow-Origin", "*");
+                .setHeader("Access-Control-Allow-Origin", "*")
+                .send(result);
         }
     } else if (mod == "preflighted") {
         if (action == "success") {
-            response(res)
-                .setHeader("Access-Control-Allow-Methods", "GET, POST")
-                .setHeader("Access-Control-Allow-Headers", "content-type")
-                .setHeader("Access-Control-Allow-Origin", "*");
+            req.method == "OPTIONS" ?
+                response(res)
+                    .setHeader("Access-Control-Allow-Methods", "GET, POST")
+                    .setHeader("Access-Control-Allow-Headers", "content-type")
+                    .setHeader("Access-Control-Allow-Origin", "*")
+                    .send(result)
+                :
+                response(res)
+                    .setHeader("Access-Control-Allow-Origin", "*")
+                    .send(result);
         } else if (action == "fail") {
             if (type == "field") {
                 response(res)
                     .setHeader("Access-Control-Allow-Methods", "GET, POST")
                     .setHeader("Access-Control-Allow-Headers", "content-type")
-                    .setHeader("Access-Control-Allow-Origin", "*");
+                    .setHeader("Access-Control-Allow-Origin", "*")
+                    .send(result);
             }
         }
     } else if (mod == "cookies") {
@@ -34,28 +43,43 @@ http.createServer(function (req, res) {
             response(res)
                 .setHeader("Access-Control-Allow-Methods", "GET, POST")
                 .setHeader("Access-Control-Allow-Headers", "content-type")
-                .setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:4000")
-                .setHeader("Access-Control-Allow-Credentials", "true");
+                .setHeader("Access-Control-Allow-Origin", "http://client.com")
+                .setHeader("Access-Control-Allow-Credentials", "true")
+                .send(req.headers.cookie ? req.headers.cookie.replace(/server=/, "") : "no cookie");
         } else if (action == "fail") {
             if (type == "origin") {
                 response(res)
                     .setHeader("Access-Control-Allow-Methods", "GET, POST")
                     .setHeader("Access-Control-Allow-Headers", "content-type")
                     .setHeader("Access-Control-Allow-Origin", "*")
-                    .setHeader("Access-Control-Allow-Credentials", "true");
+                    .setHeader("Access-Control-Allow-Credentials", "true")
+                    .send(result);
             } else if (type == "credentials") {
                 response(res)
                     .setHeader("Access-Control-Allow-Methods", "GET, POST")
                     .setHeader("Access-Control-Allow-Headers", "content-type")
-                    .setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:4000");
+                    .setHeader("Access-Control-Allow-Origin", "http://client.com")
+                    .send(result);
             }
 
-        }
+        } else if (action == "set") {
+            let value = "";
+            req.on("data", function (chunk) {
+                value += chunk;
+            });
+            req.on("end", function () {
+                response(res)
+                    .setHeader("Access-Control-Allow-Methods", "GET, POST")
+                    .setHeader("Access-Control-Allow-Headers", "content-type")
+                    .setHeader("Access-Control-Allow-Origin", "http://client.com")
+                    .setHeader("Access-Control-Allow-Credentials", "true")
+                    .setHeader("Set-Cookie", `server=${value}; domain=server.com; path=/`)
+                    .send();
+            })
+        } 
+    } else {
+        response(res).send(result);
     }
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.write('Hello, World');
-    res.end();
 }).listen(3000);
 
 function response(res) {
@@ -63,6 +87,12 @@ function response(res) {
         setHeader: (key, value) => {
             res.setHeader(key, value);
             return me;
+        },
+        send: (value) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            value && res.write(value);
+            res.end();
         }
     }
     return me;
